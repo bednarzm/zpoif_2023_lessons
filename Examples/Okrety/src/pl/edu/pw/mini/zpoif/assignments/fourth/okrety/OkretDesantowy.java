dfd08a00f55bfcd44a1be50adda77f6e4e2bf023
Maciej Bednarz <m.bednarz@mini.pw.edu.pl>
2023-11-12 18:29:33 +0100

    Komponenty do kontrolnych zadań Jachty i Okręty.

---
+++ Examples/Okrety/src/pl/edu/pw/mini/zpoif/assignments/fourth/okrety/OkretDesantowy.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.mini.zpoif.assignments.fourth.okrety;
+
+public class OkretDesantowy extends Okret {
+
+	protected float zanurzenie;
+
+	public OkretDesantowy(String nazwa, int wypornosc, float dlugosc, int zasieg, int predkosc, int mocNapedu,
+			float zanurzenie) {
+		super(nazwa, wypornosc, dlugosc, zasieg, predkosc, mocNapedu);
+		this.zanurzenie = zanurzenie;
+	}
+
+}
